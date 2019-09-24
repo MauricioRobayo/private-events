33d62ba56a819ed56988482efa23a6114e03bf21
oscarnava <oscarnava@gmail.com>
2019-09-24 16:51:59 -0500

    Added session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    return nil unless session[:user_id]
+
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user=(user)
+    if user
+      session[:user_id] = user.id
+    else
+      session.delete :user_id
+    end
+    @current_user = user || nil
+  end
+
 end
