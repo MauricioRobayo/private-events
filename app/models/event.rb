9b0a4183cebce57f4afbb60dc852b6d7a4745eaf
oscarnava <oscarnava@gmail.com>
2019-09-25 18:31:48 -0500

    Fixed issue with attendees display

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator,  foreign_key: 'creator_id', class_name: 'User'
-  has_many   :attendees, foreign_key: 'attendee_id', through: :attendances #, source: :user
+  belongs_to :creator,   foreign_key: 'creator_id',  class_name: 'User'
   has_many   :attendances
+  has_many   :attendees, foreign_key: 'attendee_id', class_name: 'User', through: :attendances #, source: :user
 end
