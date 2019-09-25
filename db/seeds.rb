c81bc7bf7cb69050ec569defab6efa15ddeadf09
Mauricio Robayo <rfmajo@gmail.com>
2019-09-25 12:08:41 -0500

    Add fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.create(username: Faker::Name.unique.first_name)
+  rand(20).times do
+    Event.create(description: Faker::Hipster.sentence, date: Faker::Date.forward(days: 30), creator_id: user.id)
+  end
+end
