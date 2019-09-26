e5b390698178fd8a58d606f1cc7bd5c5d65d9897
Mauricio Robayo <rfmajo@gmail.com>
2019-09-26 14:41:01 -0500

    Split home page into previous and upcoming

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    self.current_user = User.find_by(username: params[:session][:username])
-    @current_user = current_user
-    if current_user
-      redirect_to @current_user
+    if (self.current_user = User.find_by(username: params[:session][:username]))
+      flash[:success] = 'You are succesffully loged in.'
+      redirect_to current_user
     else
+      flash.now[:failure] = 'Invalid username. Please try again.'
       render :new
     end
   end
