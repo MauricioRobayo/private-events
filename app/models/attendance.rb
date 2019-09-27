b15d695f0d8b1328b1dac0b6c659f0a54ff87daa
oscarnava <oscarnava@gmail.com>
2019-09-27 13:01:02 -0500

    Made invited_by default to attendant

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class Attendance < ApplicationRecord
+  before_validation do |attendance|
+    attendance.invited_by = attendance.attendee if attendance.invited_by.nil?
+  end
+
   belongs_to :attendee, class_name: :User
   belongs_to :invited_by, class_name: :User
   belongs_to :attended_event, class_name: :Event, foreign_key: :event_id
+
+  validates :invited_by, presence: false
 end
