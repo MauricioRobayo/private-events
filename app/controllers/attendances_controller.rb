b0c1f85ea6226445baa3c0490082007b05714a70
Mauricio Robayo <rfmajo@gmail.com>
2019-09-27 12:23:22 -0500

    Use attendaces on current_user

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,6 @@
 class AttendancesController < ApplicationController
   def create
-    if Attendance.new(attendee: current_user, event_id: params[:event_id], invited_by: current_user).save
+    if current_user.attendances.new(event_id: params[:event_id], invited_by: current_user).save
       flash[:success] = 'You are now attending to the event!'
       redirect_to event_path(params[:event_id])
     else
